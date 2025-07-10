21ef7f39d93ee2fbee67aee0eed2ba7ba2ff952f
axelfatherofpeace <kumar1998satish@gmail.com>
2025-07-10 08:46:19 +0530

    added gitignore and excluded terraform state files

--- scripts/test_upload.sh
+++ scripts/test_upload.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#BUCKET_NAME="csv-upload-bucket-1077932e"
+BUCKET_NAME="csv-upload-bucket-573a2219"
 
 # Get the bucket name from Terraform output
-BUCKET_NAME=$(terraform -chdir=terraform output -raw s3_bucket_name)
+#TF_DIR="../terraform"
+#BUCKET_NAME=$(terraform -chdir=$TF_DIR output -raw s3_bucket_name)
 
 aws s3 cp ../sample.csv s3://$BUCKET_NAME/
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "s3_bucket_name" {
-  value = aws_s3_bucket.csv_bucket.id
+  value = aws_s3_bucket.csv_bucket.bucket
   description = "The name of the S3 bucket used for CSV upload"
 }
 
